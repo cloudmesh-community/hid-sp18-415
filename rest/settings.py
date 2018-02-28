c099c04363dcd13b1c717d2f0a6b3475058f63e1
Janaki Khatiwada <janu.khatiwada@gmail.com>
2018-02-27 22:49:52 -0500

    update rest

--- rest/settings.py
+++ rest/settings.py
@@ -1,1 +1,27 @@
-##
+MONGO_HOST = 'Localhost'
+MONGO_PORT = 27017
+MONGO_DBNAME = 'myplatform_db'
+DOMAIN = {
+    'myplatform': {
+        'schema': {
+            'sysname': {
+                'type': 'string'
+            },
+            'osystem': {
+                'type': 'string'
+            },
+            'memory': {
+                'type': 'string'
+            },
+            'processor': {
+                'type': 'string'
+            },
+            'diskspace': {
+                'type': 'string',
+                 'unique': True
+            }
+        }
+    }
+}
+RESOURCE_METHODS = ['GET','POST']
+
