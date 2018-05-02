b1252f2517579ff37db928a219600a15b111199f
Janaki Khatiwada <janu.khatiwada@gmail.com>
2018-05-01 22:23:57 -0400

    changed file

--- project-code/heroku/regapp/regressionAPI.py
+++ project-code/heroku/regapp/regressionAPI.py
@@ -6,6 +6,7 @@ from sklearn.linear_model import LinearRegression
 
 app = Flask(__name__)
 
+joblib.dump(lin_reg, "linear_regression_model.pkl")
 
 @app.route("/predict", methods=['POST'])
 def predict():
